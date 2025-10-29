ba85150736052eda5eb33640ddc5a38ddef62fc3
A-A-R-O-NWASHERE <768335@student.dusd.net>
2025-10-29 11:03:02 -0700

    add variables and functions
    we added variables and functions and we fully add and did some scripting

---
+++ P4RodriguezScriptingproject/Assets/spricts/VariablesandFunctions.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesandFunctions : MonoBehaviour
+{
+    //create my integer variable
+    int myInt = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //run my variable thru my function
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //create the multibly by two function
+    int MultiplyByTwo(int number)
+    {
+        //create a local variable
+        int result;
+
+        //multiplying the number by two
+        result = number * 2;
+
+        //return the value
+        return result;
+    }
+}
